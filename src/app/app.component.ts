b8e936f26bbeb6d8fed17bf5cdace6ae4a714f4e
lmiletic <l.miletic@msgnetconomy.net>
2019-06-18 09:41:07 +0200

    2-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
 
   @ViewChild('f', { static: false }) form: NgForm;
   defaultQuestion = 'pet';
+  answer = '';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
