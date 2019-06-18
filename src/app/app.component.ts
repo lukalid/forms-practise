39c22c569c7816ac1f59fe562d857f224ec02cb3
lmiletic <l.miletic@msgnetconomy.net>
2019-06-18 10:38:32 +0200

    Initial settup for Reactive approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,49 +7,5 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  @ViewChild('f', { static: false }) form: NgForm;
-  defaultQuestion = 'pet';
-  answer = '';
   genders = ['male', 'female'];
-  defaultGender = 'male';
-  user = {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    answer: '',
-    gender: ''
-  };
-  submitted = false;
-
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-
-    // this.form.setValue({
-    //   userData: {
-    //     username: suggestedName,
-    //     email: ''
-    //   },
-    //   secret: this.defaultQuestion,
-    //   questionAnswer: this.answer,
-    //   gender: this.defaultGender
-    // });
-
-    this.form.form.patchValue({
-      userData: {
-        username: suggestedName
-      }
-    });
-  }
-
-  onSubmit() {
-    this.submitted = true;
-    this.user.username = this.form.value.userData.username;
-    this.user.email = this.form.value.userData.email;
-    this.user.secretQuestion = this.form.value.secret;
-    this.user.answer = this.form.value.questionAnswer;
-    this.user.gender = this.form.value.gender;
-
-    this.form.reset();
-  }
 }
